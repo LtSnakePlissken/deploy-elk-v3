2058d5c8ff1e3b613470d84bb6d336970ec205b0
leodarkseid <leodarkseid@gmail.com>
2023-08-16 14:11:51 +0100

    elk v3

---
+++ src/steps/deploy-transparent-proxy-descriptor.ts
@@ -0,0 +1,17 @@
+import TransparentUpgradeableProxy from '@openzeppelin/contracts/build/contracts/TransparentUpgradeableProxy.json'
+import createDeployContractStep from './meta/createDeployContractStep'
+
+export const DEPLOY_TRANSPARENT_PROXY_DESCRIPTOR = createDeployContractStep({
+  key: 'descriptorProxyAddress',
+  artifact: TransparentUpgradeableProxy,
+  computeArguments(state) {
+    if (state.nonfungibleTokenPositionDescriptorAddressV1_3_0 === undefined) {
+      throw new Error('Missing NonfungibleTokenPositionDescriptor')
+    }
+    if (state.proxyAdminAddress === undefined) {
+      console.log("this is the proxy address",state.proxyAdminAddress)
+      throw new Error('Missing ProxyAdmin')
+    }
+    return [state.nonfungibleTokenPositionDescriptorAddressV1_3_0, state.proxyAdminAddress, '0x']
+  },
+})
